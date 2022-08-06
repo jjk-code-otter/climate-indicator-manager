1ed261b67e4d05b1dea388099814c195e4d3a00d
jjk-code-otter <jjk.code.otter@gmail.com>
2022-08-06 13:41:27 +0200

    Adding Mauna Loa to CO2

---
+++ climind/readers/reader_mauna_loa.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+import climind.data_types.timeseries as ts
+import numpy as np
+import copy
+from climind.data_manager.metadata import CombinedMetadata
+
+
+def read_ts(out_dir: Path, metadata: CombinedMetadata, **kwargs):
+    filename = out_dir / metadata['filename'][0]
+
+    construction_metadata = copy.deepcopy(metadata)
+
+    if metadata['type'] == 'timeseries':
+        if metadata['time_resolution'] == 'monthly':
+            return read_monthly_ts(filename, construction_metadata)
+        elif metadata['time_resolution'] == 'annual':
+            return read_annual_ts(filename, construction_metadata)
+        else:
+            raise KeyError(f'That time resolution is not known: {metadata["time_resolution"]}')
+
+    elif metadata['type'] == 'gridded':
+        raise NotImplementedError
+
+
+def read_monthly_ts(filename: str, metadata: CombinedMetadata):
+    years = []
+    months = []
+    anomalies = []
+
+    with open(filename, 'r') as f:
+        for i in range(53):
+            f.readline()
+        for line in f:
+            columns = line.split()
+            year = columns[0]
+            month = columns[1]
+            years.append(int(year))
+            months.append(int(month))
+            anomalies.append(float(columns[3]))
+
+    metadata['history'] = [f'Time series created from file {filename}']
+
+    return ts.TimeSeriesMonthly(years, months, anomalies, metadata=metadata)
+
+
+def read_annual_ts(filename: str, metadata: CombinedMetadata):
+    years = []
+    anomalies = []
+
+    with open(filename, 'r') as f:
+        for i in range(57):
+            f.readline()
+        for line in f:
+            columns = line.split()
+            year = columns[0]
+            years.append(int(year))
+            anomalies.append(float(columns[1]))
+
+    metadata['history'] = [f'Time series created from file {filename}']
+
+    return ts.TimeSeriesAnnual(years, anomalies, metadata=metadata)
