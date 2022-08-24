865fc69cabdb95fd59d5389e479f1a56d400b6e8
jjk-code-otter <jjk.code.otter@gmail.com>
2022-08-24 16:38:05 +0200

    Add separate cryosphere dashboard

--- scripts/build_dashboard.py
+++ scripts/build_dashboard.py
@@ -36,6 +36,14 @@ if __name__ == "__main__":
     dash_dir.mkdir(exist_ok=True)
     dash.build(Path(dash_dir))
 
+    # Cryosphere
+    json_file = ROOT_DIR / 'climind' / 'web' / 'dashboard_metadata' / 'cryosphere_indicators.json'
+    dash = Dashboard.from_json(json_file, METADATA_DIR)
+
+    dash_dir = DATA_DIR / 'ManagedData' / 'CryoDashboard'
+    dash_dir.mkdir(exist_ok=True)
+    dash.build(Path(dash_dir))
+
     # regional
     json_file = ROOT_DIR / 'climind' / 'web' / 'dashboard_metadata' / 'regional.json'
     dash = Dashboard.from_json(json_file, DATA_DIR / 'ManagedData' / 'RegionalMetadata')
