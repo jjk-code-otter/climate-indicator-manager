fe4fa6fd129e5da5b04f4aae62fba5255330cec9
jjk-code-otter <jjk.code.otter@gmail.com>
2023-08-28 14:23:39 +0200

    minor tidying

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../../climind'))
 # -- Project information -----------------------------------------------------
 
 project = 'Climind'
-copyright = '2022, John Kennedy'
+copyright = '2023, John Kennedy'
 author = 'John Kennedy'
 
 # The full version, including alpha/beta/rc tags
