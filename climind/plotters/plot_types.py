875ba77d7c73d649291b03a7bb0715f948cb9023
jjk-code-otter <jjk.code.otter@gmail.com>
2023-09-01 16:18:19 +0200

    Modified CDS fetcher to do sea level added reader and plotter

--- climind/plotters/plot_types.py
+++ climind/plotters/plot_types.py
@@ -1166,6 +1166,10 @@ def nice_map(dataset: xarray.Dataset, image_filename: Path, title: str, var: str
         wmo_levels = [-110, -90, -70, -50, -30, -10, 10, 30, 50, 70, 90, 110]
         wmo_cols = ['#543005', '#8c510a', '#bf812d', '#dfc27d', '#f6e8c3', '#f5f5f5',
                     '#c7eae5', '#80cdc1', '#35978f', '#01665e', '#003c30']
+    if var == 'sla':
+        wmo_levels = [-300, -250, -200, -150, -100, -50, 0, 50, 100, 150, 200, 250, 300]
+        wmo_cols = ['#2a0ad9', '#264dff', '#3fa0ff', '#72daff', '#aaf7ff', '#e0ffff',
+                    '#ffffbf', '#fee098', '#ffad73', '#f76e5e', '#d82632', '#a50022']
     else:
         wmo_levels = [-5, -3, -2, -1, -0.5, -0.25, 0, 0.25, 0.5, 1, 2, 3, 5]
         wmo_cols = ['#2a0ad9', '#264dff', '#3fa0ff', '#72daff', '#aaf7ff', '#e0ffff',
@@ -1188,6 +1192,8 @@ def nice_map(dataset: xarray.Dataset, image_filename: Path, title: str, var: str
     cbar.set_ticklabels(wmo_levels)
     if var == 'pre':
         cbar.set_label(r'Precipitation difference from 1981-2010 average (mm)', rotation=0, fontsize=15)
+    elif var == 'sla':
+        cbar.set_label(r'Sea level difference from long term mean average (mm)', rotation=0, fontsize=15)
     else:
         cbar.set_label(r'Temperature difference from 1981-2010 average ($\degree$C)', rotation=0, fontsize=15)
 
---
+++ climind/readers/reader_cds_sea_level.py
@@ -0,0 +1,60 @@
+#  Climate indicator manager - a package for managing and building climate indicator dashboards.
+#  Copyright (c) 2023 John Kennedy
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pathlib import Path
+from typing import Tuple
+import itertools
+import xarray as xa
+import numpy as np
+import climind.data_types.grid as gd
+import climind.data_types.timeseries as ts
+import copy
+from climind.readers.generic_reader import get_last_modified_time
+from climind.data_manager.metadata import CombinedMetadata
+
+
+def read_ts(out_dir: Path, metadata: CombinedMetadata, **kwargs):
+    construction_metadata = copy.deepcopy(metadata)
+
+    if metadata['type'] == 'gridded':
+
+        filename = out_dir
+
+        if 'grid_resolution' in kwargs:
+            if kwargs['grid_resolution'] == 5:
+                return
+            if kwargs['grid_resolution'] == 1:
+                return
+        else:
+            return read_monthly_grid(filename, construction_metadata)
+
+
+def read_monthly_grid(filename, metadata):
+    combo = read_grid(filename)
+    metadata['history'] = [f"Gridded dataset created from file {metadata['filename']} "
+                           f"downloaded from {metadata['url']}"]
+    return gd.GridMonthly(combo, metadata)
+
+
+def read_grid(filename: Path):
+    dataset_list = []
+    for year, month in itertools.product(range(1993, 2030), range(1, 13)):
+        filled_filename = filename / f"dt_global_twosat_phy_l4_{year}{month:02d}_vDT2021-M01.nc"
+        if filled_filename.exists():
+            dataset_list.append(xa.open_dataset(filled_filename))
+    combo = xa.concat(dataset_list, dim='time')
+    combo['sla'] = combo['sla'] * 1000.0
+    return combo
