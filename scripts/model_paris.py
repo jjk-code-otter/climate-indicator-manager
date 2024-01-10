9ee8aea66e3635f28e325aa55156b5ee48838bd0
jjk-code-otter <jjk.code.otter@gmail.com>
2024-01-10 14:43:17 +0100

    Adding model options

---
+++ scripts/model_paris.py
@@ -0,0 +1,72 @@
+#  Climate indicator manager - a package for managing and building climate indicator dashboards.
+#  Copyright (c) 2024 John Kennedy
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import copy
+from pathlib import Path
+import logging
+
+import climind.data_manager.processing as dm
+import climind.plotters.plot_types as pt
+import climind.stats.utils as utils
+
+from climind.data_types.timeseries import make_combined_series
+
+from climind.config.config import DATA_DIR
+from climind.definitions import METADATA_DIR
+
+if __name__ == "__main__":
+
+    final_year = 2100
+
+    project_dir = DATA_DIR / "ManagedData"
+    metadata_dir = METADATA_DIR
+
+    data_dir = project_dir / "Data"
+    fdata_dir = project_dir / "Formatted_Data"
+    figure_dir = project_dir / 'Figures'
+    log_dir = project_dir / 'Logs'
+    report_dir = project_dir / 'Reports'
+    report_dir.mkdir(exist_ok=True)
+
+    script = Path(__file__).stem
+    logging.basicConfig(filename=log_dir / f'{script}.log',
+                        filemode='w', level=logging.INFO)
+
+    # Read in the whole archive then select the various subsets needed here
+    archive = dm.DataArchive.from_directory(metadata_dir)
+
+    # some global temperature data sets are annual only, others are monthly so need to read these separately
+    ts_archive = archive.select({
+        'variable': 'tas',
+        'type': 'timeseries',
+        'time_resolution': 'monthly',
+        'origin': 'model'
+    })
+
+    all_datasets = ts_archive.read_datasets(data_dir)
+
+    all_annual_datasets = []
+    for ds in all_datasets:
+        ds.rebaseline(1981, 2010)
+        annual = ds.make_annual()
+        annual.add_offset(0.69)
+        annual.manually_set_baseline(1850, 1900)
+        annual.select_year_range(1850, final_year)
+        all_annual_datasets.append(annual)
+
+
+    pt.neat_plot(figure_dir, all_annual_datasets, 'model_annual.png', r'Global Mean Temperature Difference ($\degree$C)')
+
+
